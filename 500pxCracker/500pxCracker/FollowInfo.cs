f1073a9d39ca4c1f03f8fb359697fdad9b4b4c4e
Karol Marszałek (karomar701) <karomar701@student.polsl.pl>
2018-03-27 19:40:33 +0200

    Initial commit Essentials classes declared. I have to wonder how valid object of each of them should look like, will do in few days. Todo: Get data from webstie

---
+++ 500pxCracker/500pxCracker/FollowInfo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _500pxCracker
+{
+
+    class Statistics
+    {
+        //dont know yet, will do
+    }
+    class FollowInfo
+    {
+        protected User _Parent;
+        protected User _Target; //any ideas for better name?
+        protected Statistics _Stats;
+        protected User Parent
+        {
+            set
+            {
+                _Parent = value;
+            }
+            get
+            {
+                return _Parent;
+            }
+        }
+
+        protected User Target
+        {
+            set
+            {
+                _Target = value;
+            }
+            get
+            {
+                return _Target;
+            }
+        }
+        protected Statistics Stats
+        {
+            get
+            {
+                return _Stats;
+            }
+            set
+            {
+                _Stats = value;
+            }
+        }
+    }
+
+
+    class Follower : FollowInfo
+    {
+        public Follower(User parent, User target)
+        {
+            Parent = parent;
+            Target = target;
+        }
+
+        public void Follow()
+        {
+            //todo: implementation. Should create the Following reference between Parent and Target. 
+        }
+        
+    }
+
+    class Following: FollowInfo
+    {
+        public Following(User parent, User target)
+        {
+            Parent = parent;
+            Target = target;
+        }
+
+        public void Unfollow()
+        {
+            //todo: implementation. Should remove the Follower reference between Parent and Target. 
+        }
+    }
+}
---
+++ 500pxCracker/500pxCracker/User.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _500pxCracker
+{
+    class Credentials
+    {
+        private string _login;
+        private string _passwd;
+        public Credentials(string login, string password)
+        {
+            _login = login;
+            _passwd = password;
+        }
+        public string login
+        {
+            set
+            {
+                _login = value;
+            }
+            get
+            {
+                return _login;
+            }
+        }
+        public string password
+        {
+            set
+            {
+                _passwd = value;
+            }
+            get
+            {
+                return _passwd;
+            }
+        }
+
+    }
+
+    class References
+    {
+        private List<Following> _followings;
+        private List<Follower> _followers;
+        public List<Following> followings
+        {
+            set
+            {
+                _followings = value;
+            }
+            get
+            {
+                return _followings;
+            }
+        }
+        public List<Follower> followers
+        {
+            set
+            {
+                _followers = value;
+            }
+            get
+            {
+                return _followers;
+            }
+        }
+    }
+
+    class User
+    {
+        private int _id;
+        private Credentials _creds;
+        private References _refs;
+        public int ID
+        {
+            set
+            {
+                _id = value;
+            }
+            get
+            {
+                return _id;
+            }
+        }
+        public Credentials credentials
+        {
+            set
+            {
+                _creds = value;
+            }
+            get
+            {
+                return _creds;
+            }
+        }
+        public References references
+        {
+            set
+            {
+                _refs = value;
+            }
+            get
+            {
+                return _refs;
+            }
+        }
+    }
+}
