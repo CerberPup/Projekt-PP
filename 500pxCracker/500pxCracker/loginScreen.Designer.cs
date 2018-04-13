2e691a5aa18ebe9b342ff10dc9c9c46678c124e3
Wojtek <wojc.wrona@gmail.com>
2018-04-13 22:12:01 +0200

    Transparent

--- 500pxCracker/500pxCracker/loginScreen.Designer.cs
+++ 500pxCracker/500pxCracker/loginScreen.Designer.cs
@@ -125,6 +125,7 @@
             this.Name = "loginScreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "500px cracker";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.Load += new System.EventHandler(this.loginScreen_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.loginScreen_MouseDown);
             ((System.ComponentModel.ISupportInitialize)(this.logoPic)).EndInit();
